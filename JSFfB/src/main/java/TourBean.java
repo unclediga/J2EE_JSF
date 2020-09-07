c83b777e8b2d698f92bc2a9cad133b8fa2a99f1b
homeuser <zowie_i@mail.ru>
2020-09-07 20:05:38 +0300

    Calling methods from MBeans

---
+++ JSFfB/src/main/java/TourBean.java
@@ -0,0 +1,23 @@
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class TourBean {
+
+    private String kindOfTour;
+
+    public String getKindOfTour() {
+        return kindOfTour;
+    }
+
+    public void setKindOfTour(String kindOfTour) {
+        this.kindOfTour = kindOfTour;
+    }
+
+    public String startTour() {
+        if(kindOfTour != null && kindOfTour.equals("city")){
+            return "city_tour";
+        }else {
+            return "country_tour";
+        }
+    }
+}
