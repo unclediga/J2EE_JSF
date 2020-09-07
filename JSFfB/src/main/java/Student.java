c56e64f2eb2ca327940267650a74f6d9710aaeda
homeuser <zowie_i@mail.ru>
2020-09-07 12:19:24 +0300

    Drop-Down List 1

--- JSFfB/src/main/java/Student.java
+++ JSFfB/src/main/java/Student.java
@@ -4,6 +4,7 @@ import javax.faces.bean.ManagedBean;
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
 
     public String getFirstName() {
         return firstName;
@@ -19,6 +20,14 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     @Override
